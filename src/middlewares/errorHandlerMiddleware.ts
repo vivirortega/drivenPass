cb71e306425692202f6ae973cc978d21098bea68
Vinicius <vinigugelmin@hotmail.com>
2022-07-29 16:39:42 -0300

    Preparando revisão de código

--- src/middlewares/errorHandlerMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import {Request, Response, NextFunction} from 'express';
-
-export default async function errorHandler( error, req: Request, res: Response, next: NextFunction) {
-  console.log(error);
-  if(error.code) {
-    return res.status(error.code).send(error.message);
-  }
-  return res.sendStatus(500);
-}
--- src/schemas/wifiSchema.ts
+++
@@ -1,7 +0,0 @@
-import joi from "joi";
-import { WifiService } from "../types/genericTypes.js";
-
-export const wifiSchema = joi.object<WifiService>({
-  title: joi.string().required(),
-  password: joi.string().required(),
-});
